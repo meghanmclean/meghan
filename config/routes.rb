5ccd7f116001e93649d88275e55c5163b2039c0d
meghanmclean <mcm276@scarletmail.rutgers.edu>
2019-05-29 15:22:34 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+    #get 'users/new'
+  get '/signup', to: 'users#new'
   get 'static_pages/home'
   get 'static_pages/jobs'
   get 'static_pages/living'
   get 'static_pages/fun'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  resources :users
 end
