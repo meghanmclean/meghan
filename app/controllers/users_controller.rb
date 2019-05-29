2bbcd3bd2b45667cf2f3c9d2e4e3baed0c4b228a
meghanmclean <mcm276@scarletmail.rutgers.edu>
2019-05-29 15:42:23 -0400

    fixed issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       end
   def create
-      @user = User.new(user_parms)
+      @user = User.new(user_params)
       if @user.save
-          redicrect_to '/'
+          redirect_to '/'
           end
   end
   
